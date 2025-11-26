ef1a566033a740f696e8cdae5c995a36b349ddb7
nagma <bano.nagma.1@gmail.com>
2025-11-26 19:43:54 +0530

    implement my-counter component with increment, decrement, and reset functionality

--- ngrx/src/app/my-counter/my-counter.component.ts
+++ ngrx/src/app/my-counter/my-counter.component.ts
@@ -1,11 +1,30 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-my-counter',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './my-counter.component.html',
   styleUrl: './my-counter.component.css'
 })
 export class MyCounterComponent {
+  count$: Observable<number> | undefined;
+
+  constructor() {
+
+  }
+
+  increment() {
+
+  }
+
+  decrement() {
+
+  }
+
+  reset() {
+
+  }
 
 }
