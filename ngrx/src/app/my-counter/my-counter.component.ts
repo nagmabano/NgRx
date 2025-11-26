c48d65f58a21094439660848af7014c0ef135d68
nagma <bano.nagma.1@gmail.com>
2025-11-26 18:02:43 +0530

    my counter component creation

---
+++ ngrx/src/app/my-counter/my-counter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-counter',
+  imports: [],
+  templateUrl: './my-counter.component.html',
+  styleUrl: './my-counter.component.css'
+})
+export class MyCounterComponent {
+
+}
