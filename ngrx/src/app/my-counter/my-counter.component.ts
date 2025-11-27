ee8c3bc5eb3b455211180ed51a75584b2709ad4d
nagma <bano.nagma.1@gmail.com>
2025-11-27 13:06:41 +0530

    feat: integrate NgRx store with my-counter component for state management

--- ngrx/src/app/my-counter/my-counter.component.ts
+++ ngrx/src/app/my-counter/my-counter.component.ts
@@ -1,6 +1,9 @@
 import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { selectCount } from "../counter.reducer"
+import { decrement, increment, reset } from '../counter.actions';
 
 @Component({
   selector: 'app-my-counter',
@@ -11,20 +14,20 @@ import { Observable } from 'rxjs';
 export class MyCounterComponent {
   count$: Observable<number> | undefined;
 
-  constructor() {
-
+  constructor(private store: Store<{count: number}>) {
+    this.count$ = this.store.select(selectCount);
   }
 
   increment() {
-
+    this.store.dispatch(increment())
   }
 
   decrement() {
-
+    this.store.dispatch(decrement())
   }
 
   reset() {
-
+    this.store.dispatch(reset())
   }
 
 }
