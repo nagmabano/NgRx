398322a00cf289df942b38b8bc45dc5112d98b21
nagma <bano.nagma.1@gmail.com>
2025-11-27 13:05:46 +0530

    fix: correct styleUrls property and add standalone flag in app component

--- ngrx/src/app/app.component.ts
+++ ngrx/src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, importProvidersFrom } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MyCounterComponent } from './my-counter/my-counter.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [MyCounterComponent, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ngrx';
